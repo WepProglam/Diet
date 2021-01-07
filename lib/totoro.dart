543b5f34f948c7833bb97abf53ebce0b47ba84cc
To-ToRo <ghkdcks1223@gmail.com>
2021-01-07 21:24:25 +0900

    make infinite scroll listview

--- lib/totoro.dart
+++ lib/totoro.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: basicAppBar(),
         body: Center(
-          child: Text('HI'),
+          child: DietList(),
         ),
       ),
     );
@@ -39,4 +39,37 @@ Widget basicAppBar() {
       ),
     ],
   );
+}
+
+class DietList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //실제로는 db에서 얻어와야 함
+    final List<String> dietNameEX = [
+      'AB',
+      'BC',
+      'CD',
+      'DE',
+      'EF',
+      'FG',
+      'GH',
+      'HI',
+      'IJ'
+    ];
+
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: dietNameEX.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
+          color: Colors.grey,
+
+          //이걸 수정해서 두 개로 나눌 수 있음
+          child: Center(child: Text('${dietNameEX[index]}')),
+        );
+      },
+      separatorBuilder: (context, index) => Divider(),
+    );
+  }
 }
