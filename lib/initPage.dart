27dd6a7bceb8c4361725c14289063bae6eb460df
bw-99 <bb1702@g.skku.edu>
2021-01-07 22:09:46 +0900

    .

--- lib/initPage.dart
+++ lib/initPage.dart
@@ -13,6 +13,7 @@ class _InitPage extends State<InitPage> {
     return GestureDetector(
         onTap: () {
           setState(() {
+            selChild = !selChild;
             selected = !selected;
             Future.delayed(const Duration(milliseconds: 1200), () {
               setState(() {
@@ -25,26 +26,29 @@ class _InitPage extends State<InitPage> {
         child: Container(
           child: Center(
               child: AnimatedContainer(
-            onEnd: () {
-              Navigator.pushNamed(context, '/personalForm');
-              setState(() {
-                selected = false;
-              });
-            },
-            width: selected ? 300.0 : 100.0,
-            height: selected ? 100.0 : 300.0,
-            alignment:
-                selected ? Alignment.center : AlignmentDirectional.center,
-            duration: Duration(milliseconds: 1500),
-            color: selected ? Colors.redAccent : Colors.blueAccent,
-            curve: Curves.fastOutSlowIn,
-            child: selChild
-                ? Icon(
-                    Icons.accessibility_new,
-                    size: 75,
-                  )
-                : FlutterLogo(size: 75),
-          )),
+                  onEnd: () {
+                    Navigator.pushNamed(context, '/personalForm');
+                    setState(() {
+                      selected = false;
+                      //selChild = false;
+                    });
+                  },
+                  width: selected ? 300.0 : 100.0,
+                  height: selected ? 100.0 : 300.0,
+                  alignment:
+                      selected ? Alignment.center : AlignmentDirectional.center,
+                  duration: Duration(milliseconds: 1500),
+                  color: selected ? Colors.redAccent : Colors.blueAccent,
+                  curve: Curves.fastOutSlowIn,
+                  child: selChild
+                      ? Icon(
+                          Icons.accessibility_new,
+                          size: 75,
+                        )
+                      : Icon(
+                          Icons.accessibility,
+                          size: 75,
+                        ))),
           color: Colors.white,
         ));
     // onTap: ;
