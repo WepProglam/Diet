1955a5012e9814333516826f1ffcb7d03888a14b
To-ToRo <ghkdcks1223@gmail.com>
2021-01-08 10:19:16 +0900

    make diet list to button

--- lib/savedDiet.dart
+++ lib/savedDiet.dart
@@ -34,12 +34,19 @@ class DietList extends StatelessWidget {
     ];
 
     return GridView.count(
+      padding: EdgeInsets.all(8),
       crossAxisCount: 2,
+      //각 항목의 사이즈
+      childAspectRatio: 5 / 4,
       children: List.generate(
         dietNameEX.length,
         (index) {
           return Card(
-            child: Center(
+            margin: EdgeInsets.all(8),
+            child: FlatButton(
+              //onPressed에 식단 설정 페이지로 이동하는 함수 넣기
+              onPressed: () {},
+
               child: Text(
                 '${dietNameEX[index]}',
                 style: TextStyle(fontSize: 30),
