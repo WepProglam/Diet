cb1b72f8ab614d4892ca344db3e6cf54654505f2
bw-99 <bb1702@g.skku.edu>
2021-01-08 21:58:33 +0900

    navi bug

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -9,21 +9,7 @@ import 'package:intl/intl.dart' show DateFormat;
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'dooboolab flutter calendar',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Calendar Carousel Example'),
-    );
+    return MyHomePage(title: 'Flutter Calendar Carousel Example');
   }
 }
 
