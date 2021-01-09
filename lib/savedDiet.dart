42da52c029a3607973b87a1296e5c5f6058267e5
To-ToRo <ghkdcks1223@gmail.com>
2021-01-09 21:35:09 +0900

    transform floating action button

--- lib/savedDiet.dart
+++ lib/savedDiet.dart
@@ -12,14 +12,7 @@ class SavedDiet extends StatelessWidget {
       body: Center(
         child: DietList(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(
-          Icons.expand_less,
-          size: 40,
-        ),
-        backgroundColor: Colors.black45,
-      ),
+      floatingActionButton: TransFAB(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
@@ -64,6 +57,144 @@ class DietList extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class TransFAB extends StatefulWidget {
+  final Function() onPressed;
+  final String tooltip;
+  final IconData icon;
+
+  TransFAB({this.onPressed, this.tooltip, this.icon});
+
+  @override
+  _TransFABState createState() => _TransFABState();
+}
+
+class _TransFABState extends State<TransFAB>
+    with SingleTickerProviderStateMixin {
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _buttonColor;
+  Animation<double> _animateIcon;
+  Animation<double> _translateButton;
+  Curve _curve = Curves.easeOut;
+  double _fabHeight = 56.0;
+
+  @override
+  initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _buttonColor = ColorTween(
+      //이유는 모르겠지만 핫리로딩이 안됨 다시 실행해야 적용됨.
+      begin: Colors.black45,
+      end: Colors.orange[300],
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  animate() {
+    if (!isOpened) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+    isOpened = !isOpened;
+  }
+
+  Widget add() {
+    return Container(
+      child: FloatingActionButton(
+        heroTag: null,
+        onPressed: () {},
+        tooltip: 'Add',
+        child: Icon(Icons.add, size: 30),
+        backgroundColor: Colors.black45,
+      ),
+    );
+  }
+
+  Widget search() {
+    return Container(
+      child: FloatingActionButton(
+        heroTag: null,
+        onPressed: () {},
+        tooltip: 'Search',
+        child: Icon(Icons.search, size: 30),
+        backgroundColor: Colors.black45,
+      ),
+    );
+  }
+
+  Widget toggle() {
+    return Container(
+      child: FloatingActionButton(
+        heroTag: null,
+        backgroundColor: _buttonColor.value,
+        onPressed: animate,
+        tooltip: 'Toggle',
+        child: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          progress: _animateIcon,
+          size: 30,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value * 2.0,
+            0.0,
+          ),
+          child: search(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value,
+            0.0,
+          ),
+          child: add(),
+        ),
+        toggle(),
+      ],
     );
   }
 }
