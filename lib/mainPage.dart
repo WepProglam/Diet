4afe34172a6025d3cf0b32699f98ef55273d6df5
bw-99 <bb1702@g.skku.edu>
2021-01-10 23:42:52 +0900

    make diet block reactive & add comment

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -26,12 +26,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var date = 1;
-  var mealTime = "아침";
-  var calender_year = DateTime.now().year;
+  var mealTime = "false";
+  var calender_year = DateTime.now().year; //달력 상 표기 되는 달력
   var calender_month = DateTime.now().month;
   var daysFirstWeek;
   var daysLastWeek;
   var lastDayDateTime;
+
   @override
   Widget build(BuildContext context) {
     var dateTime = DateTime(calender_year, calender_month, 1);
@@ -43,14 +44,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     var dayLast = DateFormat('EEEE').format(lastDayDateTime);
 
-    daysLastWeek = 7 - dayToDate(dayLast);
-    print(dayLast);
-    print(dayToDate(dayLast));
-    daysFirstWeek = 7 - dayToDate(dayFirst) + 1;
-    print("datsFirstWEEK $daysFirstWeek");
+    daysLastWeek = 7 - dayToDate(dayLast); //마지막 주에 몇일 있는지
+    daysFirstWeek = 7 - dayToDate(dayFirst) + 1; //첫 주에 몇일 있는지
 
-    // var daysFirstWeek = 7 - dayToDate[dayFirst] + 1; //첫 주 요일 개수
-    print(daysFirstWeek);
     return Scaffold(
         appBar: basicAppBar('Main Page', context),
         drawer: NavDrawer(),
@@ -61,13 +57,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             calenderMonthChange(),
             calenderDayRow(), //요일
-            calenderRow(1, tag: "first"), //날짜 => 추후 정확한 정보 기입
-            calenderRow(1 + daysFirstWeek),
+            calenderRow(1, tag: "first"),
+            calenderRow(1 + daysFirstWeek), //첫주 다 채우고 새로운 주의 첫 일 ( ex) 1월 3일)
             calenderRow(8 + daysFirstWeek),
             calenderRow(15 + daysFirstWeek),
             calenderRow(22 + daysFirstWeek),
             calenderRow(29 + daysFirstWeek, tag: "end"),
-            // calenderRow(36), //익월 날짜 포함 가능위해 임시적으로 만든 자리(1월 달력에서 2월 날짜 보이는거)
             dietDate(date.toString()),
             diet(date.toString()),
             dietBox(mealTime, date),
@@ -79,6 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   int dayToDate(String day) {
+    //첫, 마지막 주에 몇일 있는지 계산하기 위해 만든 함수
     int date;
     switch (day) {
       case 'Sunday':
@@ -107,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget calenderMonthChange() {
+    //월 이동 함수
     return Expanded(
       flex: 1,
       child: Row(
@@ -114,7 +111,6 @@ class _MyHomePageState extends State<MyHomePage> {
           Spacer(
             flex: 1,
           ),
-          // Expanded(flex: 5, child: Text("2021년 1월")),
           Expanded(
               flex: 20,
               child: Row(
@@ -164,6 +160,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget calenderDayRow() {
+    //일월화수목금토
     return Expanded(
         flex: 2,
         child: Row(
@@ -194,8 +191,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget calenderRow(int date, {String tag = null}) {
-    //추후 year,month 받고 달력 달라지는 기능 추가
-
+    /*한 줄 단위로 표시 (주 X) 
+    ex) 금 토
+        일 월 화 수 목 금 토
+        ...
+        일 월 화
+    */
     var children;
     int datesFirst = daysFirstWeek;
     int datesEnd = daysLastWeek;
@@ -223,10 +224,8 @@ class _MyHomePageState extends State<MyHomePage> {
     ));
     int datesFirst = daysFirstWeek;
     int datesEnd = daysLastWeek;
-    var totalDays = lastDayDateTime.day;
-    print("daysLastweek $daysLastWeek");
-
-    int weekTotalDays = 7;
+    var totalDays = lastDayDateTime.day; //해당 월의 최대 날짜(31일, 30일 등)
+    int weekTotalDays = 7; //1주에 총 7일 존재
 
     var dt = date;
     if (tag == "first") {
@@ -261,9 +260,6 @@ class _MyHomePageState extends State<MyHomePage> {
     children.add(Spacer(
       flex: 1,
     ));
-
-    // print(children);
-
     return new Row(
       children: children,
     );
@@ -279,14 +275,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Container(
             alignment: isitDay ? null : Alignment(-1.0, -1.0),
             margin: EdgeInsets.only(top: 5.0),
-            decoration: BoxDecoration(
-                // color: Colors.white,
-                // border: isitDay
-                //     ? null
-                //     : Border(
-                //         top: BorderSide(color: Colors.blue),
-                //       )
-                ),
+            decoration: BoxDecoration(),
             child: isitDay
                 ? title
                 : Column(
@@ -311,7 +300,10 @@ class _MyHomePageState extends State<MyHomePage> {
     return Expanded(
         flex: 1,
         child: Center(
-          child: Text("$date일"),
+          child: Text(
+            "$date일",
+            style: TextStyle(fontSize: 30),
+          ),
         ));
   }
 
@@ -326,8 +318,6 @@ class _MyHomePageState extends State<MyHomePage> {
               child: FlatButton(
                 color: Colors.red,
                 child: Container(
-                  // decoration: BoxDecoration(color: Colors.white10),
-                  // border: Border(bottom: BorderSide(color: Colors.blue))),
                   child: Center(
                       child: Text(
                     '아침',
@@ -339,7 +329,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onPressed: () {
                   setState(() {
-                    mealTime = "아침";
+                    mealTime = mealTime == "아침" ? "false" : "아침";
                   });
                 },
               )),
@@ -361,7 +351,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onPressed: () {
                   setState(() {
-                    mealTime = '점심';
+                    mealTime = mealTime == "점심" ? "false" : "점심";
                   });
                 },
               )),
@@ -370,8 +360,6 @@ class _MyHomePageState extends State<MyHomePage> {
               child: FlatButton(
                 color: Colors.blue,
                 child: Container(
-                  // decoration: BoxDecoration(color: Colors.white10),
-                  // border: Border(bottom: BorderSide(color: Colors.blue))),
                   child: Center(
                       child: Text(
                     '저녁',
@@ -383,7 +371,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onPressed: () {
                   setState(() {
-                    mealTime = "저녁";
+                    mealTime = mealTime == "저녁" ? "false" : "저녁";
                   });
                 },
               ))
@@ -394,275 +382,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget dietBox(String day, int date) {
     //추후 아침 점심 저녁에 따라 표시 정보 달라질 예정 / 임시로 색깔만 바꿈
-    return Expanded(
-        flex: 7,
-        child: Container(
-          decoration: BoxDecoration(
-              color: day == "아침"
-                  ? Colors.red
-                  : day == "점심"
-                      ? Colors.green
-                      : Colors.blue),
-        ));
+
+    return mealTime != "false"
+        ? Expanded(
+            flex: 7,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: day == "아침"
+                      ? Colors.red
+                      : day == "점심"
+                          ? Colors.green
+                          : Colors.blue),
+            ))
+        : Spacer(
+            flex: 7,
+          );
   }
 }
-// 받아온 달력 주석 처리함
-// class _MyHomePageState extends State<MyHomePage> {
-//   DateTime _currentDate = DateTime(2021, 1, 8);
-//   DateTime _currentDate2 = DateTime(2021, 1, 8);
-//   String _currentMonth = DateFormat.yMMM().format(DateTime(2019, 2, 3));
-//   DateTime _targetDateTime = DateTime(2021, 1, 8);
-// //  List<DateTime> _markedDate = [DateTime(2018, 9, 20), DateTime(2018, 10, 11)];
-//   static Widget _eventIcon = new Container(
-//     decoration: new BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: BorderRadius.all(Radius.circular(1000)),
-//         border: Border.all(color: Colors.blue, width: 2.0)),
-//     child: new Icon(
-//       Icons.person,
-//       color: Colors.amber,
-//     ),
-//   );
-
-//   EventList<Event> _markedDateMap = new EventList<Event>(
-//     events: {
-//       new DateTime(2019, 2, 10): [
-//         new Event(
-//           date: new DateTime(2019, 2, 10),
-//           title: 'Event 1',
-//           icon: _eventIcon,
-//           dot: Container(
-//             margin: EdgeInsets.symmetric(horizontal: 1.0),
-//             color: Colors.red,
-//             height: 5.0,
-//             width: 5.0,
-//           ),
-//         ),
-//         new Event(
-//           date: new DateTime(2019, 2, 10),
-//           title: 'Event 2',
-//           icon: _eventIcon,
-//         ),
-//         new Event(
-//           date: new DateTime(2019, 2, 10),
-//           title: 'Event 3',
-//           icon: _eventIcon,
-//         ),
-//       ],
-//     },
-//   );
-
-//   CalendarCarousel _calendarCarousel, _calendarCarouselNoHeader;
-
-//   @override
-//   void initState() {
-//     /// Add more events to _markedDateMap EventList
-//     _markedDateMap.add(
-//         new DateTime(2019, 2, 25),
-//         new Event(
-//           date: new DateTime(2019, 2, 25),
-//           title: 'Event 5',
-//           icon: _eventIcon,
-//         ));
-
-//     _markedDateMap.add(
-//         new DateTime(2019, 2, 10),
-//         new Event(
-//           date: new DateTime(2019, 2, 10),
-//           title: 'Event 4',
-//           icon: _eventIcon,
-//         ));
-
-//     _markedDateMap.addAll(new DateTime(2019, 2, 11), [
-//       new Event(
-//         date: new DateTime(2019, 2, 11),
-//         title: 'Event 1',
-//         icon: _eventIcon,
-//       ),
-//       new Event(
-//         date: new DateTime(2019, 2, 11),
-//         title: 'Event 2',
-//         icon: _eventIcon,
-//       ),
-//       new Event(
-//         date: new DateTime(2019, 2, 11),
-//         title: 'Event 3',
-//         icon: _eventIcon,
-//       ),
-//     ]);
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     /// Example with custom icon
-//     _calendarCarousel = CalendarCarousel<Event>(
-//       onDayPressed: (DateTime date, List<Event> events) {
-//         this.setState(() => _currentDate = date);
-//         events.forEach((event) => print(event.title));
-//       },
-//       weekendTextStyle: TextStyle(
-//         color: Colors.red,
-//       ),
-//       thisMonthDayBorderColor: Colors.grey,
-// //          weekDays: null, /// for pass null when you do not want to render weekDays
-//       headerText: 'Custom Header',
-//       weekFormat: true,
-//       markedDatesMap: _markedDateMap,
-//       height: 200.0,
-//       selectedDateTime: _currentDate2,
-//       showIconBehindDayText: true,
-// //          daysHaveCircularBorder: false, /// null for not rendering any border, true for circular border, false for rectangular border
-//       customGridViewPhysics: NeverScrollableScrollPhysics(),
-//       markedDateShowIcon: true,
-//       markedDateIconMaxShown: 2,
-//       selectedDayTextStyle: TextStyle(
-//         color: Colors.yellow,
-//       ),
-//       todayTextStyle: TextStyle(
-//         color: Colors.blue,
-//       ),
-//       markedDateIconBuilder: (event) {
-//         return event.icon;
-//       },
-//       minSelectedDate: _currentDate.subtract(Duration(days: 360)),
-//       maxSelectedDate: _currentDate.add(Duration(days: 360)),
-//       todayButtonColor: Colors.transparent,
-//       todayBorderColor: Colors.green,
-//       markedDateMoreShowTotal:
-//           true, // null for not showing hidden events indicator
-// //          markedDateIconMargin: 9,
-// //          markedDateIconOffset: 3,
-//     );
-
-//     /// Example Calendar Carousel without header and custom prev & next button
-//     _calendarCarouselNoHeader = CalendarCarousel<Event>(
-//       todayBorderColor: Colors.green,
-//       onDayPressed: (DateTime date, List<Event> events) {
-//         this.setState(() => _currentDate2 = date);
-//         events.forEach((event) => print(event.title));
-//       },
-//       daysHaveCircularBorder: true,
-//       showOnlyCurrentMonthDate: false,
-//       weekendTextStyle: TextStyle(
-//         color: Colors.red,
-//       ),
-//       thisMonthDayBorderColor: Colors.grey,
-//       weekFormat: false,
-// //      firstDayOfWeek: 4,
-//       markedDatesMap: _markedDateMap,
-//       height: 420.0,
-//       selectedDateTime: _currentDate2,
-//       targetDateTime: _targetDateTime,
-//       customGridViewPhysics: NeverScrollableScrollPhysics(),
-//       markedDateCustomShapeBorder:
-//           CircleBorder(side: BorderSide(color: Colors.yellow)),
-//       markedDateCustomTextStyle: TextStyle(
-//         fontSize: 18,
-//         color: Colors.blue,
-//       ),
-//       showHeader: false,
-//       todayTextStyle: TextStyle(
-//         color: Colors.blue,
-//       ),
-//       // markedDateShowIcon: true,
-//       // markedDateIconMaxShown: 2,
-//       // markedDateIconBuilder: (event) {
-//       //   return event.icon;
-//       // },
-//       // markedDateMoreShowTotal:
-//       //     true,
-//       todayButtonColor: Colors.yellow,
-//       selectedDayTextStyle: TextStyle(
-//         color: Colors.yellow,
-//       ),
-//       minSelectedDate: _currentDate.subtract(Duration(days: 360)),
-//       maxSelectedDate: _currentDate.add(Duration(days: 360)),
-//       prevDaysTextStyle: TextStyle(
-//         fontSize: 16,
-//         color: Colors.pinkAccent,
-//       ),
-//       inactiveDaysTextStyle: TextStyle(
-//         color: Colors.tealAccent,
-//         fontSize: 16,
-//       ),
-//       onCalendarChanged: (DateTime date) {
-//         this.setState(() {
-//           _targetDateTime = date;
-//           _currentMonth = DateFormat.yMMM().format(_targetDateTime);
-//         });
-//       },
-//       onDayLongPressed: (DateTime date) {
-//         print('long pressed date $date');
-//       },
-//     );
-
-//     return GestureDetector(
-//         onTap: () {
-//           // Navigator.pushNamed(context, '/personalForm');
-//         },
-//         child: Scaffold(
-//             appBar: basicAppBar(widget.title, context),
-//             drawer: NavDrawer(),
-//             body: SingleChildScrollView(
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 mainAxisAlignment: MainAxisAlignment.start,
-//                 children: <Widget>[
-//                   //custom icon
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 16.0),
-//                     child: _calendarCarousel,
-//                   ), // This trailing comma makes auto-formatting nicer for build methods.
-//                   //custom icon without header
-//                   Container(
-//                     margin: EdgeInsets.only(
-//                       top: 30.0,
-//                       bottom: 16.0,
-//                       left: 16.0,
-//                       right: 16.0,
-//                     ),
-//                     child: new Row(
-//                       children: <Widget>[
-//                         Expanded(
-//                             child: Text(
-//                           _currentMonth,
-//                           style: TextStyle(
-//                             fontWeight: FontWeight.bold,
-//                             fontSize: 24.0,
-//                           ),
-//                         )),
-//                         FlatButton(
-//                           child: Text('PREV'),
-//                           onPressed: () {
-//                             setState(() {
-//                               _targetDateTime = DateTime(_targetDateTime.year,
-//                                   _targetDateTime.month - 1);
-//                               _currentMonth =
-//                                   DateFormat.yMMM().format(_targetDateTime);
-//                             });
-//                           },
-//                         ),
-//                         FlatButton(
-//                           child: Text('NEXT'),
-//                           onPressed: () {
-//                             setState(() {
-//                               _targetDateTime = DateTime(_targetDateTime.year,
-//                                   _targetDateTime.month + 1);
-//                               _currentMonth =
-//                                   DateFormat.yMMM().format(_targetDateTime);
-//                             });
-//                           },
-//                         )
-//                       ],
-//                     ),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 16.0),
-//                     child: _calendarCarouselNoHeader,
-//                   ), //
-//                 ],
-//               ),
-//             )));
-//   }
-// }
