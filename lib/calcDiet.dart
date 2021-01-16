802a5e5db212b78ca385e645b9c7f45ef156e5d7
bw-99 <bb1702@g.skku.edu>
2021-01-17 00:41:10 +0900

    add calcdiet page

--- lib/calcDiet.dart
+++ lib/calcDiet.dart
@@ -7,13 +7,60 @@ class CalcDiet extends StatefulWidget {
 }
 
 class _CalcDietState extends State<CalcDiet> {
+  List<bool> isItVisible = [false, false, false, false];
+  List<String> meal = ["아침", "점심", "저녁", "간식"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: basicAppBar('calcDiet', context),
         drawer: NavDrawer(),
         body: Container(
-          child: Text('hello world'),
+          child: Column(
+            children: [
+              returnDietButton(0),
+              returnDietButton(1),
+              returnDietButton(2),
+              returnDietButton(3),
+            ],
+          ),
+        ));
+  }
+
+  Widget returnDietButton(int index) {
+    return Expanded(
+      flex: 1,
+      child: Row(
+        children: [diet(index), addDiet(index)],
+      ),
+    );
+  }
+
+  Widget diet(int index) {
+    return FlatButton(
+        onPressed: () {
+          setState(() {
+            isItVisible[index] = !isItVisible[index];
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+          ),
+          child: Text(
+            meal[index],
+            style: TextStyle(fontSize: 50),
+          ),
+        ));
+  }
+
+  Widget addDiet(int index) {
+    return Visibility(
+        visible: isItVisible[index],
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+          ),
+          child: Text(meal[index]),
         ));
   }
 }
