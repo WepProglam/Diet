90bf1a0c6031f12896a716f011f63d8144ab595d
bw-99 <bb1702@g.skku.edu>
2021-01-17 01:02:48 +0900

    calcDiet.dart > diet add or modify option

--- lib/calcDiet.dart
+++ lib/calcDiet.dart
@@ -7,8 +7,14 @@ class CalcDiet extends StatefulWidget {
 }
 
 class _CalcDietState extends State<CalcDiet> {
-  List<bool> isItVisible = [false, false, false, false];
-  List<String> meal = ["아침", "점심", "저녁", "간식"];
+  Map visibleMeal = {
+    "아침": {"visiblity": false, "added": false},
+    "점심": {"visiblity": false, "added": false},
+    "저녁": {"visiblity": false, "added": false},
+    "간식": {"visiblity": false, "added": false}
+  };
+
+  List<String> foods = ["현미밥", "닭가슴살", "감자"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,56 +23,74 @@ class _CalcDietState extends State<CalcDiet> {
         body: Container(
           child: Column(
             children: [
-              returnDietButton(0),
-              returnDietButton(1),
-              returnDietButton(2),
-              returnDietButton(3),
+              returnDietButton("아침"),
+              returnDietButton("점심"),
+              returnDietButton("저녁"),
+              returnDietButton("간식"),
             ],
           ),
         ));
   }
 
-  Widget returnDietButton(int index) {
+  Widget returnDietButton(String key) {
     return Expanded(
       flex: 1,
       child: Row(
-        children: [diet(index), addDiet(index)],
+        children: [diet(key), addDiet(key)],
       ),
     );
   }
 
-  Widget diet(int index) {
+  Widget diet(String key) {
     return FlatButton(
         onPressed: () {
           setState(() {
-            isItVisible[index] = !isItVisible[index];
+            visibleMeal[key]["visiblity"] = !visibleMeal[key]["visiblity"];
           });
         },
         child: Container(
           decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-          ),
+              // border: Border.all(color: Colors.black),
+              ),
           child: Text(
-            meal[index],
+            key,
             style: TextStyle(fontSize: 50),
           ),
         ));
   }
 
-  Widget addDiet(int index) {
+  Widget addDiet(String key) {
     return AnimatedOpacity(
         // If the widget is visible, animate to 0.0 (invisible).
         // If the widget is hidden, animate to 1.0 (fully visible).
-        opacity: isItVisible[index] ? 1.0 : 0.0,
+        opacity: visibleMeal[key]["visiblity"] ? 1.0 : 0.0,
         duration: Duration(milliseconds: 300),
         // The green box must be a child of the AnimatedOpacity widget.
         child: Container(
-          width: 300,
-          height: 200,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-          ),
-          child: Text(meal[index]),
-        ));
+            width: 300,
+            height: 200,
+            decoration: BoxDecoration(
+                // border: Border.all(color: Colors.black),
+                ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton(
+                  child: Center(
+                    child: Text(
+                      visibleMeal[key]["added"] ? "수정하기" : "추가하기",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  onPressed: () {
+                    visibleMeal[key]["added"] = true;
+                    Navigator.pushNamed(context, '/savedDiet');
+                  },
+                ),
+                for (var i in foods) Text(i + "  249 Kcal\n"),
+              ],
+            )));
   }
 }
