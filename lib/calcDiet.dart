c138af909ad085e7dbe15d46428c05781fee4bc1
bw-99 <bb1702@g.skku.edu>
2021-01-18 02:41:00 +0900

    calc diet ui jot gat ne

--- lib/calcDiet.dart
+++ lib/calcDiet.dart
@@ -8,10 +8,10 @@ class CalcDiet extends StatefulWidget {
 
 class _CalcDietState extends State<CalcDiet> {
   Map visibleMeal = {
-    "아침": {"visiblity": false, "added": false},
-    "점심": {"visiblity": false, "added": false},
-    "저녁": {"visiblity": false, "added": false},
-    "간식": {"visiblity": false, "added": false}
+    "아침": {"visiblity": true, "added": false},
+    "점심": {"visiblity": true, "added": false},
+    "저녁": {"visiblity": true, "added": false},
+    "간식": {"visiblity": true, "added": false}
   };
 
   List<String> foods = ["현미밥", "닭가슴살", "감자"];
@@ -23,22 +23,19 @@ class _CalcDietState extends State<CalcDiet> {
         body: Container(
           child: Column(
             children: [
-              returnDietButton("아침"),
-              returnDietButton("점심"),
-              returnDietButton("저녁"),
-              returnDietButton("간식"),
+              Spacer(
+                flex: 1,
+              ),
+              addDiet("아침"),
+              addDiet("점심"),
+              addDiet("저녁"),
+              addDiet("간식"),
+              Spacer(
+                flex: 1,
+              ),
             ],
           ),
         ));
-  }
-
-  Widget returnDietButton(String key) {
-    return Expanded(
-      flex: 1,
-      child: Row(
-        children: [diet(key), addDiet(key)],
-      ),
-    );
   }
 
   Widget diet(String key) {
@@ -63,41 +60,46 @@ class _CalcDietState extends State<CalcDiet> {
 
   Widget addDiet(String key) {
     return Expanded(
-        flex: 4,
-        child: AnimatedOpacity(
-            // If the widget is visible, animate to 0.0 (invisible).
-            // If the widget is hidden, animate to 1.0 (fully visible).
-            opacity: visibleMeal[key]["visiblity"] ? 1.0 : 0.0,
-            duration: Duration(milliseconds: 300),
-            // The green box must be a child of the AnimatedOpacity widget.
-            child: Container(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FlatButton(
-                  child: Center(
-                    child: Text(
-                      visibleMeal[key]["added"] ? "수정하기" : "추가하기",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-                    ),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      visibleMeal[key]["added"] = true;
-                    });
+        flex: 3,
+        child: Row(children: [
+          Spacer(
+            flex: 1,
+          ),
+          Expanded(
+              flex: 8,
+              child: Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueAccent)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      FlatButton(
+                        child: ListTile(
+                            leading: Text(key),
+                            trailing: Text(
+                              visibleMeal[key]["added"] ? "수정하기" : "추가하기",
+                            )),
+                        onPressed: () {
+                          setState(() {
+                            visibleMeal[key]["added"] = true;
+                          });
 
-                    Navigator.pushNamed(context, '/savedDiet');
-                  },
-                ),
-                for (var i in foods)
-                  Container(
-                    child: Center(child: Text(" " + i + "  249 Kcal\n")),
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.black)),
-                  ),
-              ],
-            ))));
+                          Navigator.pushNamed(context, '/savedDiet');
+                        },
+                      ),
+                      for (var i in foods)
+                        Container(
+                          child: Center(child: Text(" " + i + "  249 Kcal\n")),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                        ),
+                    ],
+                  ))),
+          Spacer(
+            flex: 1,
+          )
+        ]));
   }
 }
